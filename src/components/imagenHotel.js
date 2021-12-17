30364b5867b1982c47cf2653edbaee7f89d06aa4
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-17 00:25:16 -0500

    Adding css to the Hero

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -19,13 +19,41 @@ const ImagenHotel = () => {
       }
     }
   `)
-  console.log(image)
+  const TextoImagen = styled.div`
+    background-image: linear-gradient(
+      to top,
+      rgba(34, 49, 63, 0.75),
+      rgba(34, 49, 63, 0.75)
+    );
+    color: #fff;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      font-size: 4rem;
+      margin: 0;
+      @media (min-width: 992px) {
+        font-size: 5.8rem;
+      }
+    }
+    p {
+      font-size: 2rem;
+      @media (min-width: 992px) {
+        font-size: 2.6rem;
+      }
+    }
+  `
   return (
-    <ImageBackground
-      fadeIn="soft"
-      tag="section"
-      fluid={image.sharp.fluid}
-    ></ImageBackground>
+    <ImageBackground fadeIn="soft" tag="section" fluid={image.sharp.fluid}>
+      <TextoImagen>
+        <h1>Bienvenido a Hotel Gatsby</h1>
+        <p>El mejor hotel para tus vacaciones</p>
+      </TextoImagen>
+    </ImageBackground>
   )
 }
 
