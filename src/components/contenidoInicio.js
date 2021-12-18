a60646fc9cafbdaaeb9ade87263cd90927f2fcfe
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-17 19:27:12 -0500

    getting the data from datocms

---
+++ src/components/contenidoInicio.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+const ContenidoInicio = () => {
+  const information = useStaticQuery(graphql`
+    query {
+      allDatoCmsPagina(filter: { slug: { eq: "inicio" } }) {
+        nodes {
+          titulo
+          contenido
+          imagen {
+            fluid {
+              ...GatsbyDatoCmsFluid
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(information)
+  return (
+    <>
+      <h2>Titulo de pagina</h2>
+    </>
+  )
+}
+
+export default ContenidoInicio
