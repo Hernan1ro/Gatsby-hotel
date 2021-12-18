5089d6be184009fdee3c8c274965b9d163ecb562
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-18 10:28:29 -0500

    adding gatsby-image to optimize the imgs

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
 
 const ContenidoInicio = () => {
   const information = useStaticQuery(graphql`
@@ -23,7 +24,7 @@ const ContenidoInicio = () => {
       <h2>{titulo}</h2>
       <div>
         <p>{contenido}</p>
-        <img src={imagen.fluid.src} alt="Hotel" />
+        <Image fluid={imagen.fluid} alt="Hotel" />
       </div>
     </>
   )
