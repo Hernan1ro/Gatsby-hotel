0f852f358514798d61ea91f13e3ac70abdbaf46b
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-17 19:26:24 -0500

    Getting the data from datoCms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import ImagenHotel from "../components/imagenHotel"
+import ContenidoInicio from "../components/contenidoInicio"
 
 const IndexPage = () => {
   return (
     <Layout>
       <ImagenHotel />
+      <ContenidoInicio />
     </Layout>
   )
 }
