97bac2406472d3dffe57f9a7314c2085d781d6a3
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-19 09:22:53 -0500

    styling room page

--- src/components/habitaciones.js
+++ src/components/habitaciones.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "gatsby-image"
+import { css } from "@emotion/react"
 
 export const query = graphql`
   query ($slug: String!) {
@@ -27,8 +28,21 @@ const HabitacionTemplate = ({
   const { titulo, contenido, imagen } = nodes[0]
   return (
     <Layout>
-      <main>
-        <h1>{titulo}</h1>
+      <main
+        css={css`
+          margin: 0 auto;
+          max-width: 1200px;
+          width: 95%;
+        `}
+      >
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 4rem;
+          `}
+        >
+          {titulo}
+        </h1>
         <p>{contenido}</p>
         <Image fluid={imagen.fluid} />
       </main>
