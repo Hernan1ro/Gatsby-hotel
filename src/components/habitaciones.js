6ca8269a9b913d1d9bd96acb32f7ff3840090b88
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-19 08:58:36 -0500

    making a query to fetch the data for each page

--- src/components/habitaciones.js
+++ src/components/habitaciones.js
@@ -1,6 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const HabitacionTemplate = () => {
+export const query = graphql`
+  query ($slug: String!) {
+    allDatoCmsHabitacione(filter: { slug: { eq: $slug } }) {
+      nodes {
+        titulo
+        contenido
+        imagen {
+          fluid(maxWidth: 1200) {
+            ...GatsbyDatoCmsFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+const HabitacionTemplate = ({ data }) => {
+  console.log(data)
   return (
     <div>
       <h1>Habitaciones</h1>
